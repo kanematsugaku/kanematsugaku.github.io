065f462b65805239135404764ae51e68ef910079
kanematsugaku <kanematsu.gaku@gmail.com>
2021-02-19 00:53:34 +0900

    deploy

--- micanga/_next/static/chunks/webpack.js
+++ micanga/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7282d5de19d04bb464e3";
+/******/ 	var hotCurrentHash = "8642ce47c30af84b80cc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
